7390721189274ff6cbd525af59bd28cd56d836e9
William Rodriguez <william@rodriguez1.com>
2022-06-03 09:30:25 -0700

    Add home page

---
+++ Sweets/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Sweets.Models;
+
+namespace Sweets.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly SweetsContext _db;
+
+    public HomeController(SweetsContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
